8146053c523c47a3e2fb1e8a8adab501a7d647a7
Isabela Saenz <isabelasaenzcardoso@gmail.com>
2026-09-02 20:35:06 -0300

    Commit 6: Moving method get_charge from Rental to Book

--- model.py
+++ model.py
@@ -7,6 +7,20 @@ class Book:
         self.title = title
         self.price_code = price_code
 
+    def get_charge(self, days_rented: int) -> float:
+        amount = 0
+        if self.price_code == Book.REGULAR:
+            amount += 2
+            if days_rented > 2:
+                amount += (days_rented - 2) * 1.5
+        elif self.price_code == Book.NEW_RELEASE:
+            amount += days_rented * 3
+        elif self.price_code == Book.CHILDREN:
+            amount += 1.5
+            if days_rented > 3:
+                amount += (days_rented - 3) * 1.5
+        return amount
+
 
 class Rental:
     def __init__(self, book, days_rented):
@@ -14,26 +28,42 @@ class Rental:
         self.days_rented = days_rented
 
     def get_charge(self) -> float:
-        amount = 0
-        if self.book.price_code == Book.REGULAR:
-            amount += 2
-            if self.days_rented > 2:
-                amount += (self.days_rented - 2) * 1.5
-        elif self.book.price_code == Book.NEW_RELEASE:
-            amount += self.days_rented * 3
-        elif self.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if self.days_rented > 3:
-                amount += (self.days_rented - 3) * 1.5
-        return amount
+        return self.book.get_charge(self.days_rented)
 
     def get_frequent_renter_points(self):
-        # CORREÇÃO: O método deve somar ao invés de atribuir
         points = 1
         if self.book.price_code == Book.NEW_RELEASE and self.days_rented > 1:
             points += 1
         return points
 
+
+class Client:
+    def __init__(self, name):
+        self.name = name
+        self.rentals = []
+
+    def add_rental(self, rental):
+        self.rentals.append(rental)
+
+    def statement(self) -> str:
+        total_amount = 0
+        frequent_renter_points = 0
+        result = f"Rental summary for {self.name}\n"
+
+        for rental in self.rentals:
+            amount = rental.get_charge()
+
+            # add frequent renter points
+            frequent_renter_points += rental.get_frequent_renter_points()
+
+            # show each rental result
+            result += f"- {rental.book.title}: {amount}\n"
+            total_amount += amount
+
+        # show total result
+        result += f"Total: {total_amount}\n"
+        result += f"Points: {frequent_renter_points}"
+        return result
 
 class Client:
     def __init__(self, name):
